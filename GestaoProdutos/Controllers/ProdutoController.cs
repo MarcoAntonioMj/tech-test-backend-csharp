240336e3c728cffb74db4bbdc06e1e701a49436c
Marco Antonio <marcotonho38@gmail.com>
2023-08-11 19:27:33 -0300

    Mudando estrutura do projeto alterando pastas

---
+++ GestaoProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using GestaoProdutos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+
+namespace GestaoProdutos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private static List<Produto> produtos = new List<Produto>();
+        private static int id = 0;
+
+        [HttpPost]
+        public IActionResult AdicionarProduto([FromBody] Produto produto)
+        {
+            produto.Id = id++;
+            produtos.Add(produto);
+            return CreatedAtAction(nameof(ListaProdutoPorId), new { id = produto.Id }, produto);
+
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Produto> ListarProdutos([FromQuery] int skip = 0,
+        [FromQuery] int take = 15)
+        {
+            return produtos.Skip(skip).Take(take);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult ListaProdutoPorId(int id)
+        {
+            var produto = produtos.FirstOrDefault(produto => produto.Id == id);
+            if (produto == null) return NotFound();
+            return Ok(produto);
+        }
+    }
+}
