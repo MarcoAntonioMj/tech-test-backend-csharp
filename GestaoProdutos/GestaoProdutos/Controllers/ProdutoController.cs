0a0f58cccfa9448d280a9a013c566cd2b68b0c76
Marco Antonio <marcotonho38@gmail.com>
2023-08-10 21:12:08 -0300

    feat: Adicionar validação ao modelo Produto e atualizar controller Produto
    Adicionei validações ao modelo Produto para garantir que campos como Nome, Preço,
    QuantidadeEmEstoque e DataDeCriacao sejam obrigatórios e que o campo Preço não
    aceite valores negativos. Também atualizei o controlador Produto para refletir
    essas alterações, certificando-me de que a API responda corretamente com mensagens
    de erro quando as validações falham.

---
+++ GestaoProdutos/GestaoProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using GestaoProdutos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GestaoProdutos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private static List<Produto> produtos = new List<Produto>();
+
+        [HttpPost]
+        public void AdicionarProduto([FromBody] Produto produto)
+        {
+            produtos.Add(produto);
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco);
+            Console.WriteLine(produto.QuantidadeEmEstoque);
+            Console.WriteLine(produto.DataDeCriacao);
+
+        }
+    }
+}
+
